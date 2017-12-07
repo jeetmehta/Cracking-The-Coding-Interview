abd56dafe093be13e00064beb2762e5a03b0abef
Jeet Mehta <jeet25.m@gmail.com>
2017-12-07 15:30:01 -0500

    2.2: Return Kth to Last - known size implementation

---
+++ Linked Lists/return-kth-to-last.py
@@ -0,0 +1,77 @@
+# CTCI - Chapter 2: Linked Lists #
+# Question 2.2
+# PROBLEM STATEMENT: Implement an algorithm to find the kth to last element of a singly linked list.
+# HINTS: #8, #25, #41, #67, #126
+
+class Node:
+
+	def __init__(self):
+		self.data = 0
+		self.next = None
+
+class LinkedList:
+
+	def __init__(self):
+		self.size = 0
+		self.head = None
+
+	def addNode(self, data):
+		if (self.head == None):
+			self.head = Node()
+			self.head.data = data
+		else:
+			temp = self.head
+			while (temp.next != None):
+				temp = temp.next
+
+			temp.next = Node()
+			temp.next.data = data
+		self.size += 1
+
+	def printList(self):
+		if (self.head != None):
+			temp = self.head
+			while (temp != None):
+				print temp.data,
+				temp = temp.next
+			print
+
+	def getKthToLast(self, k):
+
+		# Edge Case 1: K is too large (can't be greater than size)
+		if (k > self.size):
+			print "ERROR: K is larger than the size of the linked list"
+			return None
+
+		# Edge Case 2: K is too small/negative
+		elif (k < 0):
+			print "ERROR: K must be greater than or equal to zero"
+			return None
+
+		count = 0
+		temp = self.head
+		while (count != self.size - k and temp.next != None):
+			temp = temp.next
+			count += 1
+
+		return temp
+
+
+
+
+# Main function - runs unit tests
+def main():
+
+	linkedList = LinkedList()
+
+	testInput = [2, 3, 5, 1, 3, 1, 9, 24, 3, 6, 1, 3]
+	for num in testInput:
+		linkedList.addNode(num)
+
+	linkedList.printList()
+	print linkedList.getKthToLast(0).data
+
+
+# Prevents file from running as import
+if __name__ == "__main__":
+	main()
