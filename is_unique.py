a37560f38af6965814450e33e81454ee56c0f522
Jeet Mehta <jeet25.m@gmail.com>
2017-12-07 11:15:15 -0500

    Iterative/Brute force approach also implemented

--- is_unique.py
+++ is_unique.py
@@ -3,7 +3,7 @@
 # PROBLEM STATEMENT: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 # HINTS: #44, #117, #132
 
-# Check if a string has all unique characters using a set (don't need a count, just needs to check if it exists)
+# Checks if a string has all unique characters using a set (don't need a count, just needs to check if it exists): O(n) where n = size of string
 def allUniqueCharactersUsingHash(inputString):
 	
 	# Edge Case 1: Empty String
@@ -23,14 +23,39 @@ def allUniqueCharactersUsingHash(inputString):
 	# All characters are unique
 	return True
 
+# Checks if a string has all unique characters iteratively, cross-checking every character against every other character: O(n^2) where n = size of string
+def allUniqueCharactersNoDS(inputString):
+
+	# Edge Case 1: Empty String
+	if inputString == "":
+		return True
+
+	# Go through every character, and check against every other character in string
+	for i, char in enumerate(inputString):
+		for j, otherChar in enumerate(inputString):
+			if (char == otherChar and i != j):
+				return False
+
+	# All characters are unique
+	return True
+
+
 # Main function - runs unit tests
 def main():
 
-	inputString = ""
+	inputString = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+~`{;'<>?}|-./"
+	print "----- Using Hash -------"
 	if (allUniqueCharactersUsingHash(inputString)):
-		print "Using Hash: String has all unique characters"
+		print " String has all unique characters"
 	else:
-		print "Using Hash: String has repeated characters"
+		print "String has repeated characters"
+
+	print "------ No DS --------"
+	if (allUniqueCharactersNoDS(inputString)):
+		print " String has all unique characters"
+	else:
+		print "String has repeated characters"
+
 
 # Prevents file from running as import
 if __name__ == "__main__":
