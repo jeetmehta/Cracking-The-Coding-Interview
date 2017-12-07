e6e26ba2dd99b250fa4f1e5bed3d4147b8f82197
Jeet Mehta <jeet25.m@gmail.com>
2017-12-07 11:37:42 -0500

    Restructuring

---
+++ Linked Lists/remove-dups.py
@@ -0,0 +1,5 @@
+# CTCI - Chapter 2: Linked Lists #
+# Question 2.1
+# PROBLEM STATEMENT: Write code to remove duplicates from an unsorted linked list. 
+# How would you solve this problem if a temporary buffer is not allowed?
+# HINTS: #9, #40
