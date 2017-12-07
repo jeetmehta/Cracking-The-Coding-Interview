d06d5c17b03b1f8b3ef2b67ea9b36b3557de7601
Jeet Mehta <jeet25.m@gmail.com>
2017-12-07 13:21:12 -0500

    2.1: Removing Duplicates - using a hash set to identify + remove duplicate values

--- Linked Lists/remove-dups.py
+++ Linked Lists/remove-dups.py
@@ -3,3 +3,83 @@
 # PROBLEM STATEMENT: Write code to remove duplicates from an unsorted linked list. 
 # How would you solve this problem if a temporary buffer is not allowed?
 # HINTS: #9, #40
+
+# Basic node class
+class Node:
+
+	def __init__(self):
+		self.data = 0
+		self.next = None
+
+# Linked list class description
+class LinkedList:
+
+	# Constructor
+	def __init__(self):
+		self.size = 0
+		self.head = None
+
+	# Add node to list
+	def addNode(self, data):
+		if (self.head == None):
+			self.head = Node()
+			self.head.data = data
+		else:
+			temp = self.head
+			while (temp.next != None):
+				temp = temp.next
+
+			temp.next = Node()
+			temp.next.data = data
+		
+		self.size += 1
+
+	# Prints list values
+	def printList(self):
+		if (self.head != None):
+			temp = self.head
+			while (temp != None):
+				print temp.data,
+				temp = temp.next
+			print
+
+	# Removes all duplicates using a hash set (keeps only one unique instance)
+	def removeDuplicates(self):
+		if (self.head != None):
+			
+			# Declare hash set		
+			hashSet = set([])
+			
+			# Initialize iterators (current + previous)
+			temp = self.head
+			previous = temp
+
+			# Iterate through list looking for and removing duplicates
+			while (temp != None):
+				if temp.data in hashSet:
+					previous.next = temp.next # should be deleting the node?
+					self.size -= 1
+				else:
+					hashSet.add(temp.data)
+					# Increment previous pointer only if no duplicates are found
+					previous = temp
+				
+				# Increment temp
+				temp = temp.next
+
+# Main function - runs unit tests
+def main():
+
+	testInput = [0, 0, 0]
+	linkedList = LinkedList()
+
+	for num in testInput:
+		linkedList.addNode(num)
+	
+	linkedList.printList()
+	linkedList.removeDuplicates()
+	linkedList.printList()
+
+# Prevents file from running as import
+if __name__ == "__main__":
+	main()
