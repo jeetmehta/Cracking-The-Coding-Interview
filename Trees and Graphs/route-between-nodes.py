2d590c5a40cebe7b6512e52af7d70fe1786357ac
Jeet Mehta <jeet25.m@gmail.com>
2017-12-08 12:54:03 -0500

    Starting the next chapter

---
+++ Trees and Graphs/route-between-nodes.py
@@ -0,0 +1,4 @@
+# CTCI - Chapter 4: Trees and Graphs #
+# Question 4.1
+# PROBLEM STATEMENT: Given a directed graph, design an algorithm to find out whether there is a route between two nodes.
+# HINTS: #127
